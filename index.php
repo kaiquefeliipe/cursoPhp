f5d875433eb7b3527498c44412c55af1f7362586
Kaique Felipe <kaique.felipe.67@gmail.com>
2017-07-23 16:08:43 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php //TAG DE ABERTURA DO PHP
+
+echo "Meu primeiro código PHP! Hello World";
+
+/*TAG DE FECHAMENTO DO PHP*/ 
+/*
+* Se o arquivo for somente em PHP, não precisa da TAG de fechamento,
+* caso não, precisa da TAG de fechamento, delimitando onde começa e onde termina
+*/
+
+?>
